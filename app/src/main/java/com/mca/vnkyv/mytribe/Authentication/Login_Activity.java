ae1f467f33e21596defdf65820c076fe171d5df5
naeem-2005 <naeemkhan7d@gmail.com>
2024-01-20 12:15:12 +0530

    Added 4 activities to start work

---
+++ app/src/main/java/com/mca/vnkyv/mytribe/Authentication/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.mca.vnkyv.mytribe.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mca.vnkyv.mytribe.R;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
