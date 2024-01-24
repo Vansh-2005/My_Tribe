318db80f37c69d74f1e4df3d51f02ed222939e97
naeem-2005 <naeemkhan7d@gmail.com>
2024-01-24 13:52:41 +0530

    Bottom navigation and drwaer layout setuo is complete.

--- app/src/main/java/com/mca/vnkyv/mytribe/MainActivity.java
+++ app/src/main/java/com/mca/vnkyv/mytribe/MainActivity.java
@@ -1,14 +1,106 @@
 package com.mca.vnkyv.mytribe;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.dashboard_item);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
+    public void onBackPressed() {
+
+
+        if (bottomNavigationView.getSelectedItemId() == R.id.navigation_home) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Do you want to exit?");
+            builder.setCancelable(true);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    dialogInterface.cancel();
+
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.dashboard_share) {
+            Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.dashboard_rate_us) {
+            Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.dashboard_about_us) {
+            Toast.makeText(this, "newproject", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.dashboard_privacy) {
+            Toast.makeText(this, "activity", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.dashboard_term) {
+            Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
 }
