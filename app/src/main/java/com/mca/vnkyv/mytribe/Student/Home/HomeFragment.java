b309f9ba47d2dcca3e33f304deb74e8bb842bcaa
naeem-2005 <naeemkhan7d@gmail.com>
2024-02-22 12:12:36 +0530

    voice recognition enabled successfull.

--- app/src/main/java/com/mca/vnkyv/mytribe/Student/Home/HomeFragment.java
+++ app/src/main/java/com/mca/vnkyv/mytribe/Student/Home/HomeFragment.java
@@ -2,7 +2,10 @@ package com.mca.vnkyv.mytribe.Student.Home;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,6 +21,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,12 +34,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mca.vnkyv.mytribe.R;
+import com.mca.vnkyv.mytribe.Student.AddButton.Community.Add_Community_Activity;
+import com.mca.vnkyv.mytribe.Student.AddButton.Event.Add_Event_Activity;
+import com.mca.vnkyv.mytribe.Student.AddButton.Project.Add_Project_Activity;
+import com.mca.vnkyv.mytribe.Student.Profile.ProfileFragment;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
     private ActionBar actionBar;
     private FirebaseAuth mAuth;
     private DatabaseReference userReference;
+    private SpeechRecognizer speechRecognizer;
+    private final int REQUEST_CODE = 100;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,7 +62,7 @@ public class HomeFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         userReference = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
-
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
 
 
         userReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -76,6 +92,7 @@ public class HomeFragment extends Fragment {
 
         return view;
     }
+
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.home_toolbar_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
@@ -90,9 +107,42 @@ public class HomeFragment extends Fragment {
             startActivity(i);
             return true;
         }
+        if (id == R.id.voice_search) {
+            startVoiceRecognition();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void startVoiceRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String text = result.get(0);
+                executeVoiceCommand(text);
+            }
+        }
+    }
+
     private String capitalizeFirstLetter(String input) {
         if (input == null || input.isEmpty()) {
             return input;
@@ -100,4 +150,35 @@ public class HomeFragment extends Fragment {
         return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 
+    private void executeVoiceCommand(String text) {
+        text = text.toLowerCase();
+
+        if (text.contains("create project")) {
+            Intent i = new Intent(getActivity(), Add_Project_Activity.class);
+            startActivity(i);
+            Toast.makeText(getActivity(), "Create your own project here!!", Toast.LENGTH_SHORT).show();
+        } else if (text.contains("create event")) {
+            Intent i = new Intent(getActivity(), Add_Event_Activity.class);
+            startActivity(i);
+            Toast.makeText(getActivity(), "Create your own event here!!", Toast.LENGTH_SHORT).show();
+        } else if (text.contains("create community")) {
+            Intent i = new Intent(getActivity(), Add_Community_Activity.class);
+            startActivity(i);
+            Toast.makeText(getActivity(), "Create your own community here!!", Toast.LENGTH_SHORT).show();
+            }
+        else if (text.contains("open google")) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+            startActivity(intent);
+            }
+        else if (text.contains("profile")) {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.frame_layout);
+            navController.navigate(R.id.navigation_profile);
+        } else if (text.contains("my activity")) {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.frame_layout);
+            navController.navigate(R.id.navigation_activity);
+        } else {
+            // If the command is not recognized, show a message to the user
+            Toast.makeText(getActivity(), "Command not recognized", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
