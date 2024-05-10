5c541d0da8642f44d8b2af7bb5ab6478430810a6
naeem-2005 <naeemkhan7d@gmail.com>
2024-05-10 18:13:26 +0530

    updated all code

--- app/src/main/java/com/mca/vnkyv/mytribe/Student/Home/HomeFragment.java
+++ app/src/main/java/com/mca/vnkyv/mytribe/Student/Home/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.mca.vnkyv.mytribe.Student.Home;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,10 +23,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,14 +39,23 @@ import com.mca.vnkyv.mytribe.R;
 import com.mca.vnkyv.mytribe.Student.AddButton.Community.Add_Community_Activity;
 import com.mca.vnkyv.mytribe.Student.AddButton.Event.Add_Event_Activity;
 import com.mca.vnkyv.mytribe.Student.AddButton.Project.Add_Project_Activity;
-import com.mca.vnkyv.mytribe.Student.Profile.ProfileFragment;
+import com.mca.vnkyv.mytribe.Student.My_Activity.Community;
+import com.mca.vnkyv.mytribe.Student.My_Activity.CommunityAdapter;
+import com.mca.vnkyv.mytribe.Student.My_Activity.Event;
+import com.mca.vnkyv.mytribe.Student.My_Activity.EventAdapter;
+import com.mca.vnkyv.mytribe.Student.My_Activity.Project;
+import com.mca.vnkyv.mytribe.Student.My_Activity.ProjectAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
     private ActionBar actionBar;
+    private DatabaseReference projectsReference;
+    private DatabaseReference communitiesReference;
+    private DatabaseReference eventsReference;
     private FirebaseAuth mAuth;
     private DatabaseReference userReference;
     private SpeechRecognizer speechRecognizer;
@@ -54,17 +65,49 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
         Drawable d = getResources().getDrawable(R.drawable.toolbar);
         actionBar.setBackgroundDrawable(d);
         setHasOptionsMenu(true);
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         userReference = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireActivity());
 
+        if (currentUser != null) {
+            projectsReference = FirebaseDatabase.getInstance().getReference("New project");
+            communitiesReference = FirebaseDatabase.getInstance().getReference("New community");
+            eventsReference = FirebaseDatabase.getInstance().getReference("New event");
+        }
 
+        RecyclerView recyclerViewProjects = view.findViewById(R.id.project_recyclerView1);
+        recyclerViewProjects.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        ProjectAdapter projectAdapter = new ProjectAdapter(requireContext(), new ArrayList<>());
+        recyclerViewProjects.setAdapter(projectAdapter);
+
+        RecyclerView recyclerViewCommunities = view.findViewById(R.id.community_recyclerView1);
+        recyclerViewCommunities.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        CommunityAdapter communityAdapter = new CommunityAdapter(requireContext(), new ArrayList<>());
+        recyclerViewCommunities.setAdapter(communityAdapter);
+
+        RecyclerView recyclerViewEvents = view.findViewById(R.id.Event_recyclerView1);
+        recyclerViewEvents.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        EventAdapter eventAdapter = new EventAdapter(requireContext(), new ArrayList<>());
+        recyclerViewEvents.setAdapter(eventAdapter);
+
+        // Retrieve Projects and update the adapter
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            // Call the methods with the userId
+            retrieveProjects(projectAdapter);
+            retrieveCommunities(communityAdapter);
+            retrieveEvents(eventAdapter);
+        }
+
+        // Set ActionBar title and window settings
         userReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -84,22 +127,89 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-        Window window = getActivity().getWindow();
+        // Set window status bar color
+        Window window = requireActivity().getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.home_notificationbar_color));
+        window.setStatusBarColor(ContextCompat.getColor(requireActivity(), R.color.home_notificationbar_color));
 
         return view;
     }
 
+    private void retrieveProjects(ProjectAdapter adapter) {
+        projectsReference.orderByChild("uid").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Project> projectList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Project project = snapshot.getValue(Project.class);
+                    if (project != null) {
+                        projectList.add(project);
+                    }
+                }
+                adapter.setProjectList(projectList);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("ActivityFragment", "onCancelled: " + databaseError.getMessage());
+            }
+        });
+    }
+
+    private void retrieveCommunities(CommunityAdapter adapter) {
+        communitiesReference.orderByChild("uid").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Community> communityList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Community community = snapshot.getValue(Community.class);
+                    if (community != null) {
+                        communityList.add(community);
+                    }
+                }
+                adapter.setCommunityList(communityList);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("ActivityFragment", "onCancelled: " + databaseError.getMessage());
+            }
+        });
+    }
+
+    private void retrieveEvents(EventAdapter adapter) {
+        eventsReference.orderByChild("uid").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Event> eventList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Event event = snapshot.getValue(Event.class);
+                    if (event != null) {
+                        eventList.add(event);
+                    }
+                }
+                adapter.setEventList(eventList);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("ActivityFragment", "onCancelled: " + databaseError.getMessage());
+            }
+        });
+    }
+
+    @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.home_toolbar_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.search_image) {
@@ -165,11 +275,11 @@ public class HomeFragment extends Fragment {
             Intent i = new Intent(getActivity(), Add_Community_Activity.class);
             startActivity(i);
             Toast.makeText(getActivity(), "Create your own community here!!", Toast.LENGTH_SHORT).show();
-            }
+        }
         else if (text.contains("open google")) {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
             startActivity(intent);
-            }
+        }
         else if (text.contains("profile")) {
             NavController navController = Navigation.findNavController(requireActivity(), R.id.frame_layout);
             navController.navigate(R.id.navigation_profile);
--- app/src/main/java/com/mca/vnkyv/mytribe/Student/Search/SearchFragment.java
+++ app/src/main/java/com/mca/vnkyv/mytribe/Student/Search/SearchFragment.java
@@ -1,39 +1,57 @@
 package com.mca.vnkyv.mytribe.Student.Search;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toast;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mca.vnkyv.mytribe.R;
+import com.mca.vnkyv.mytribe.Student.My_Activity.Project;
+import com.mca.vnkyv.mytribe.Student.My_Activity.ProjectAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
 
+    private EditText searchEditText;
+    private DatabaseReference projectReference;
+    private DatabaseReference communityReference;
+    private List<Project> projectList;
+    private RecyclerView recyclerView;
+    private ProjectAdapter projectAdapter;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
 
-        ColorDrawable colorDrawable  = new ColorDrawable(Color.parseColor("#2c2f49"));
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#2c2f49"));
         actionBar.setBackgroundDrawable(colorDrawable);
         actionBar.setTitle("Search");
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -42,11 +60,101 @@ public class SearchFragment extends Fragment {
 
         Window window = getActivity().getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(getActivity(),R.color.profile_notificationbar_color));
+        window.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.profile_notificationbar_color));
 
+        searchEditText = view.findViewById(R.id.searchEditText);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        projectList = new ArrayList<>();
+        projectAdapter = new ProjectAdapter(getActivity(), projectList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(projectAdapter);
+
+        projectReference = FirebaseDatabase.getInstance().getReference("New project");
+        communityReference = FirebaseDatabase.getInstance().getReference("New community");
+
+        // Load default sample data
+        loadSampleData();
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                search(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
 
         return view;
+    }
+
+    private void loadSampleData() {
+        // Load sample data from "New project"
+        projectReference.limitToFirst(4).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Project project = snapshot.getValue(Project.class);
+                    projectList.add(project);
+                }
+                projectAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), "Failed to load sample data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void search(String searchText) {
+        projectList.clear();
+
+        // Query "New project" for the search text
+        projectReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Project project = snapshot.getValue(Project.class);
+                    // Check if the project title contains the search text
+                    if (project.getTitle().toLowerCase().contains(searchText.toLowerCase())) {
+                        projectList.add(project);
+                    }
+                }
+                projectAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), "No data found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Query "New community" for the search text
+        communityReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    // Assuming the community object has a title field
+                    String title = snapshot.child("title").getValue(String.class);
+                    if (title != null && title.toLowerCase().contains(searchText.toLowerCase())) {
+                        // Create a Project object to maintain consistency with projectList
+                        Project project = new Project();
+                        project.setTitle(title);
+                        projectList.add(project);
+                    }
+                }
+                projectAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), "No data found", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
